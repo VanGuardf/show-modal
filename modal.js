ef4296cb09c323406677c015241d7ea30c52b473
VanGuard <alisaadati18000@gmail.com>
2023-10-10 22:57:42 +0330

    Compelete The Project

--- modal.js
+++ modal.js
@@ -3,7 +3,8 @@ const modal = document.querySelector(".modal-div");
 const backdrop = document.querySelector(".backdrop");
 const closeModal = document.querySelector(".close-modal");
 const confirmModal = document.querySelector(".confirm-modal");
-
+const userName = document.querySelector("#userName");
+console.log(userName.value);
 show.addEventListener("click", () => {
   modal.style.transform = "translateY(1vh)";
 });
@@ -11,10 +12,14 @@ show.addEventListener("click", () => {
 function closeModalFunc() {
   modal.style.transform = "translateY(-100vh)";
 }
-
 closeModal.addEventListener("click", closeModalFunc);
 backdrop.addEventListener("click", closeModalFunc);
+
 confirmModal.addEventListener("click", () => {
-  modal.style.transform = "translateY(-100vh)";
-  alert("Confirmem Your Form!");
+  if (userName.value == "") {
+    alert("Please Enter Your Name!");
+  } else {
+    modal.style.transform = "translateY(-100vh)";
+    alert("Welcome " + userName.value);
+  }
 });
